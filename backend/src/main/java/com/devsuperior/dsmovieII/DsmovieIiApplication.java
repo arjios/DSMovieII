a30f616cafc803e69b789bf00f05adef485980d4
Arjios <antonio.uab.ios.fisica@gmail.com>
2022-03-08 17:44:32 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/dsmovieII/DsmovieIiApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieIiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmovieII/DsmovieIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
