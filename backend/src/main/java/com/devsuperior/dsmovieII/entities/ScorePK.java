86d894e28031a66306bd32d9603d9fcd304118d0
Arjios <antonio.uab.ios.fisica@gmail.com>
2022-03-10 14:17:06 -0300

    First commit

--- backend/src/main/java/com/devsuperior/dsmovieII/entities/ScorePK.java
+++ backend/src/main/java/com/devsuperior/dsmovieII/entities/ScorePK.java
@@ -36,4 +36,36 @@ public class ScorePK implements Serializable {
 	public void setUser(User user) {
 		this.user = user;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((movie == null) ? 0 : movie.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ScorePK other = (ScorePK) obj;
+		if (movie == null) {
+			if (other.movie != null)
+				return false;
+		} else if (!movie.equals(other.movie))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+	
 }
